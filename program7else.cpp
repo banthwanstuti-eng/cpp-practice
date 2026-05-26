ed5f6172f0ec4f07910402cfb9ed5c0ab4145f26
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-05-26 21:43:56 +0530

    program7else

---
+++ program7else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n1, n2;
+cout<<"enter two numbers:";
+cin>>n1>>n2;
+if 
+(n1>n2)
+cout<<"n1 is greater than n2";
+else
+cout<<"n2 is greater";
+return 0;
+}
