556ce5bc9e80ce47ee12a3f076ddb263c52c2eb7
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-05-26 22:54:40 +0530

    program8a-nested

---
+++ program8a-nested.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"enter number:";
+    cin>>marks;
+    if 
+    (marks>=90)
+    
+        cout<<"A grade";
+    
+    else if (marks<90 && marks>=75)
+    cout<<"B+ grade";
+    else if (marks<75 && marks>=60)
+    cout<<"B grade";
+    else
+    cout<<"C grade";
+    return 0;
+
+}
