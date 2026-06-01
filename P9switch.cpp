75f61c7b84df57dc2eb84e738c64c87a33d65ac1
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-06-01 23:11:47 +0530

    P9switch

---
+++ P9switch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cout<<"enter the number of day:";
+    cin>>day;
+    switch(day){
+        case 1:
+        cout<<"sunday";
+        break;
+        case 2:
+        cout<<"monday";
+        break;
+        case 3:
+        cout<<"tuesday";
+        break;
+        case 4:
+        cout<<"wednesday";
+        break;
+        case 5:
+        cout<<"thursday";
+        break;
+        case 6:
+        cout<<"friday";
+        break;
+        case 7:
+        cout<<"saturday";
+        break;
+        default:
+        cout<<"invalid day";
+        
+    }
+
+}
