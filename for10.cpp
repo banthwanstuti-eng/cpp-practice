ac0caf9dcd40502b745dcd1b11dae511de91ca0b
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-06-04 17:08:34 +0530

    for10

---
+++ for10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool prime=true;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            prime=false;
+            break;
+        }}
+        if(prime && n>1)
+        cout<<"prime";
+        else
+        cout<<"not prime";
+
+    
+    return 0;
+}
