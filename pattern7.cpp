324ed257ea64e1be4982a73cc45075ae8ab92052
banthwanstuti-eng <banthwanstuti@gmail.com>
2026-06-20 23:35:39 +0530

    pattern7

---
+++ pattern7.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows = 5;
+
+    // Upper pyramid
+    for(int i = 1; i <= rows; i++)
+    {
+        for(int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+
+        for(int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower inverted pyramid
+    for(int i = rows - 1; i >= 1; i--)
+    {
+        for(int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+
+        for(int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
